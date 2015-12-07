953d83457256602bf467bbf725840517b33b4d60
Kolek Schoonhoven <idealbuilder71@gmail.com>
2015-12-07 04:44:55 +0100

    change

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/getS3Options', function (req, res) {
 		});
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
 
   var host = server.address().address
   var port = server.address().port
