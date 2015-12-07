17dc3cc16b71258a69fc6a56b2056df226729421
Kolek Schoonhoven <idealbuilder71@gmail.com>
2015-12-07 04:39:26 +0100

    initial

---
+++ server.js
@@ -0,0 +1,44 @@
+crypto = require("crypto");
+moment = require("moment"); //requires >= 1.5.0 for UTC support
+
+var express = require('express');
+var app = express();
+
+app.use("/",express.static(__dirname + '/public'));
+
+var AWS_BUCKET = 'lezyeoh';
+var AWS_ACCESS_KEY = 'AKIAIEW5ODTJENQ2PM4Q';
+var AWS_SECRET_KEY = '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';
+
+app.get('/getS3Options', function (req, res) {
+
+	var s3Policy = {
+		'conditions': [
+			{'bucket': AWS_BUCKET},
+			['starts-with', '$key', ''],
+			{'acl': 'public-read'},
+			["content-length-range", 0, 10 * 1024 * 1024],
+			['starts-with', '$Content-Type', 'image']
+		],
+		'expiration': moment().add('minutes', 60).format("YYYY-MM-DDTHH:MM:ss\\Z")
+	};
+
+	var policy = new Buffer(JSON.stringify(s3Policy)).toString("base64")
+	var signature = crypto.createHmac("sha1", AWS_SECRET_KEY).update(policy).digest("base64")
+
+	res.json(
+		{
+			policy: policy,
+			signature: signature,
+			key: AWS_ACCESS_KEY
+		});
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+});
